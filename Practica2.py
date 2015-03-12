0f9eb3dcc017177af3cbeac77f21c82906b3e14c
ignasigm <ignasiguillen@gmail.com>
2015-03-12 13:29:11 +0100

    Update Practica2.py

--- Practica2.py
+++ Practica2.py
@@ -18,6 +18,23 @@ class Stack:
 	def __len__(self):
 		return len(self.__items)
 		
+
+def testStack():
+	llista_s = Stack()
+	print "Esta buida: ",llista_s.isEmpty()
+	llista_s.push(1)
+	llista_s.push(2)
+	llista_s.push(3)
+	print "Esta buida: ", llista_s.isEmpty()
+	print "Mida: ",len(llista_s)
+	print "Tota la llista: ",llista_s
+	llista_s.peek()
+	print "Mida: ",len(llista_s)
+	print "Element tret: ",llista_s.pop()
+	print "Tota la llista: ",llista_s
+	
+
+		
 class Queue:
 	“””Define un TDA Cola”””
 	def __init__(self):        
@@ -33,7 +50,22 @@ class Queue:
 	def __len__():        
 		return len(self.__data)
 		
-class PriorityQueue:
+def testQueue():
+	llista_q = Queue()
+	print "Esta buida: ",llista_q.isEmpty()
+	llista_q.enqueue(1)
+	llista_q.enqueue(2)
+	llista_q.enqueue(3)
+	print "Esta buida: ", llista_q.isEmpty()
+	print "Mida: ",len(llista_q)
+	print "Tota la llista: ",llista_q
+	llista_q.peek()
+	print "Mida: ",len(llista_q)
+	print "Element tret: ",llista_q.dequeue()
+	print "Tota la llista: ",llista_q
+	
+
+class PriorityQueue(Queue):
   """ Cola de prioridad, hereda de una cola pero inserta los elementos en orden, por defecto ascendiente"""        
 
 	def __init__(self):        
@@ -50,3 +82,19 @@ class PriorityQueue:
 			    		inserted=True  
 		    		i=i+1              
 		  	if not(inserted):  self.insert(len(self),el)
+def testPriorityQueue():
+	llista_qr = PriorityQueue()
+	print "Esta buida: ",llista_qr.isEmpty()
+	llista_qr.enqueue(3)
+	llista_qr.enqueue(1)
+	llista_qr.enqueue(2)
+	print "Esta buida: ", llista_qr.isEmpty()
+	print "Mida: ",len(llista_qr)
+	print "Tota la llista: ",llista_qr
+	llista_qr.peek()
+	print "Mida: ",len(llista_qr)
+	print "Element tret: ",llista_qr.dequeue()
+	print "Tota la llista: ",llista_qr
+testStack()
+testQueue()
+testPriorityQueue()
