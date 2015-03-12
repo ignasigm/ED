3ce5c53b3a8c5fa60ca9bfc12609b8f07b981de8
ignasigm <ignasiguillen@gmail.com>
2015-03-12 13:08:02 +0100

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -17,3 +17,36 @@ class Stack:
 	  	else: return None
 	def __len__(self):
 		return len(self.__items)
+		
+class Queue:
+	“””Define un TDA Cola”””
+	def __init__(self):        
+		self.__data=[]      
+	def __str__(self):        
+		return ''.join(str(elem)+','+for elem in self._data)                
+	def enqueue(self,el):        
+		self.__data.append(el)    
+	def dequeue(self):      
+		return self.__data.pop(0)    
+	def isEmpty(self):      
+		return self.__data==[]       
+	def __len__():        
+		return len(self.__data)
+		
+class PriorityQueue:
+  """ Cola de prioridad, hereda de una cola pero inserta los elementos en orden, por defecto ascendiente"""        
+
+	def __init__(self):        
+		Queue.__init__(self)          
+  
+	def enqueue(self,el):        
+		if self.isEmpty(): self.insert(0,el)        
+		elif el<=self.__getitem__(0): self.insert(0,el)        
+		else:               
+		  inserted,i=False,0              
+		  while i< (len(self)-1) and not(inserted):                
+		    if self.__getitem__(i)<el and el<=self.__getitem__(i+1): 
+			    self.insert(i+1,el)
+			    inserted=True  
+		    i=i+1              
+		  if not(inserted):  self.insert(len(self),el)
