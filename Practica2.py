fe0008bc99af12e3c2a49bac88172fb03f8dff50
ignasigm <ignasiguillen@gmail.com>
2015-03-12 13:11:41 +0100

    Update Practica2.py

--- Practica2.py
+++ Practica2.py
@@ -43,10 +43,10 @@ class PriorityQueue:
 		if self.isEmpty(): self.insert(0,el)        
 		elif el<=self.__getitem__(0): self.insert(0,el)        
 		else:               
-		  inserted,i=False,0              
-		  while i< (len(self)-1) and not(inserted):                
-		    if self.__getitem__(i)<el and el<=self.__getitem__(i+1): 
-			    self.insert(i+1,el)
-			    inserted=True  
-		    i=i+1              
-		  if not(inserted):  self.insert(len(self),el)
+			inserted,i=False,0              
+		  	while i< (len(self)-1) and not(inserted):                
+		    		if self.__getitem__(i)<el and el<=self.__getitem__(i+1): 
+			    		self.insert(i+1,el)
+			    		inserted=True  
+		    		i=i+1              
+		  	if not(inserted):  self.insert(len(self),el)
