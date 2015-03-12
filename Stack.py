c9f2b490967a3143c681e8b12075dbbc56cc2b33
ignasigm <ignasiguillen@gmail.com>
2015-03-12 12:53:55 +0100

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,10 +1,10 @@
 #class Stack
 class Stack:
-  “””Define un TDA Pila “””
+  	“””Define un TDA Pila “””
 	def __init__(self):
 		self.__items = []
 	def __str__(self):
-	  return ''.join(str(elem)+', '+for elem in self.__items)
+		return ''.join(str(elem)+', '+for elem in self.__items)
 	def isEmpty(self):
 		return self.__items == []
 	def push(self, item): 		
@@ -12,8 +12,8 @@ class Stack:
 	def pop(self):
 		return self.__items.pop()
 	def peek(self):
-	  if len(self)>0:
-	    return self.__items[len(self)-1]
-	  else: return None
+	  	if len(self)>0:
+	    		return self.__items[len(self)-1]
+	  	else: return None
 	def __len__(self):
 		return len(self.__items)
