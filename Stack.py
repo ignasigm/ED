8ff4af4a5a454f00a74d05a19bff7c3fb32c6ad0
ignasigm <ignasiguillen@gmail.com>
2015-03-12 12:53:01 +0100

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,3 +1,19 @@
 #class Stack
 class Stack:
-  
+  “””Define un TDA Pila “””
+	def __init__(self):
+		self.__items = []
+	def __str__(self):
+	  return ''.join(str(elem)+', '+for elem in self.__items)
+	def isEmpty(self):
+		return self.__items == []
+	def push(self, item): 		
+		self.__items.append(item)
+	def pop(self):
+		return self.__items.pop()
+	def peek(self):
+	  if len(self)>0:
+	    return self.__items[len(self)-1]
+	  else: return None
+	def __len__(self):
+		return len(self.__items)
